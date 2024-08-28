f4cc101c3f49f3c7831239fecde45215453b347e
rishitttt <144487460+rishitttt@users.noreply.github.com>
2024-08-29 00:36:23 +0530

    qwe234

--- app/page.js
+++ app/page.js
@@ -7,8 +7,11 @@ export default function Home() {
   const [litecoinData, setLitecoinData] = useState(null);
   const [latestTransactions, setLatestTransactions] = useState([]);
   const [marketData, setMarketData] = useState({});
-  const router = useRouter();
+  const [blockchainData, setBlockchainData] = useState({});
+  const [transactionHash, setTransactionHash] = useState('');
+  const [transactionData, setTransactionData] = useState(null);
   const [showButton, setShowButton] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     async function fetchLitecoinData() {
@@ -29,9 +32,13 @@ export default function Home() {
       const transactionsResponse = await fetch('https://api.blockchair.com/litecoin/transactions?limit=5');
       const transactionsData = await transactionsResponse.json();
 
+      const blockchainResponse = await fetch('https://api.blockchair.com/litecoin/stats');
+      const blockchainData = await blockchainResponse.json();
+
       setLitecoinData(priceData.litecoin);
       setMarketData(marketInfo);
       setLatestTransactions(transactionsData.data);
+      setBlockchainData(blockchainData.data);
 
       window.addEventListener('scroll', handleScroll);
       return () => {
@@ -41,6 +48,14 @@ export default function Home() {
 
     fetchLitecoinData();
   }, []);
+
+  const handleSearch = async () => {
+    if (transactionHash) {
+      const response = await fetch(`https://api.blockchair.com/litecoin/raw/transaction/${transactionHash}`);
+      const data = await response.json();
+      setTransactionData(data.data[transactionHash]);
+    }
+  };
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -77,33 +92,33 @@ export default function Home() {
         </div>
       </div>
 
-      <h2 className=" text-center text-4xl font-extrabold text-white font-display">Latest Transactions</h2>
+      <h2 className=" text-center text-4xl font-extrabold text-white font-display">Litecoin Details</h2>
 
       {litecoinData && (
         <div className="text-center relative z-10 w-3/4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-4">
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-brightness-125 backdrop-blur-lg p-8 h-52 rounded-lg shadow-lg transition-transform transition-all transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-brightness-125 backdrop-blur-lg p-8 rounded-lg shadow-lg transition-transform transition-all transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">Price</h2>
             <p className="text-3xl font-bold text-green-500">${litecoinData.usd}</p>
           </div>
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">Market Cap</h2>
             <p className="text-3xl font-bold text-green-500">${litecoinData.usd_market_cap.toLocaleString()}</p>
           </div>
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">24h Volume</h2>
             <p className="text-3xl font-bold text-green-500">${litecoinData.usd_24h_vol.toLocaleString()}</p>
           </div>
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">24h Change</h2>
             <p className={`text-3xl font-bold ${litecoinData.usd_24h_change >= 0 ? 'text-green-500' : 'text-red-500'}`}>
               {litecoinData.usd_24h_change.toFixed(2)}%
             </p>
           </div>
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">Circulating Supply</h2>
             <p className="text-3xl font-bold text-green-500">{marketData.market_data?.circulating_supply?.toLocaleString()} LTC</p>
           </div>
-          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+          <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <h2 className="text-2xl font-extrabold text-white font-display">Max Supply</h2>
             <p className="text-3xl font-bold text-green-500">{marketData.market_data?.max_supply?.toLocaleString()} LTC</p>
           </div>
@@ -116,7 +131,7 @@ export default function Home() {
           <h2 className="text-center text-4xl font-extrabold text-white font-display">Latest Transactions</h2>
             <div className="max-w-screen-xl mx-auto mt-8 grid grid-cols-1 gap-8 px-4">
               {latestTransactions.map((transaction, index) => (
-              <div key={index} className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg text-white transition-transform transform hover:scale-105">
+              <div key={index} className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg text-white transition-transform transform hover:scale-105">
               <p className="font-display text-lg break-all"><strong>Transaction Hash:</strong> {transaction.hash}</p>
               <p className="font-display text-lg"><strong>Block Height:</strong> {transaction.block_id}</p>
               <p className="font-display text-lg"><strong>Transaction Fee:</strong> {transaction.fee} LTC</p>
@@ -128,23 +143,53 @@ export default function Home() {
         </div>
         <div className=''>
           <div className=''>
+            <div className="relative z-10 mt-16">
+              <h2 className="text-center text-4xl font-extrabold text-white font-display">Search Transaction</h2>
+              <div className='relative w-full'><input
+                type="text"
+                placeholder="Enter Transaction Hash"
+                value={transactionHash}
+                onChange={(e) => setTransactionHash(e.target.value)}
+                className="mt-8 p-8 transition-transform transform hover:scale-105 placeholder:text-center w-3/5 p-2 mt-4 rounded-lg bg-[#87729c] text-white placeholder-gray-300"
+              /></div>
+              <button
+                onClick={handleSearch}
+                className="mt-4 px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+              >
+                Search
+              </button>
+            </div>
 
+            {transactionData && (
+            <div className="relative text-center w-3/4 mt-8 p-8 bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+              <h2 className="text-2xl font-extrabold text-white font-display">Transaction Details</h2>
+              <p className="font-display text-lg break-all"><strong>Transaction Hash:</strong> {transactionHash}</p>
+              <p className="font-display text-lg"><strong>Block Height:</strong> {transactionData.block_id}</p>
+              <p className="font-display text-lg"><strong>Transaction Fee:</strong> {transactionData.fee} LTC</p>
+              <p className="font-display text-lg"><strong>Time:</strong> {new Date(transactionData.time).toLocaleString()}</p>
+              {/* Add more details as needed */}
+            </div>
+           )}
           </div>
           <div className=''>
             <div className="relative z-10 mt-16">
             <h2 className="text-center text-4xl font-extrabold text-white font-display">Blockchain Info</h2>
-              <div className="max-w-screen-xl mx-auto mt-8 grid grid-cols-1 gap-8 px-4">
-                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+              <div className="w-3/4 mx-auto mt-8 grid grid-cols-1 gap-8 px-4">
+                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
                   <h2 className="text-2xl font-extrabold text-white font-display">Total Blocks</h2>
-                  <p className="text-3xl font-bold text-green-500">{marketData.blockchain_stats_24_hours?.blocks}</p>
+                  <p className="text-3xl font-bold text-green-500">{blockchainData.blocks}</p>
                 </div>
-                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+                  <h2 className="text-2xl font-extrabold text-white font-display">24h Transactions</h2>
+                  <p className="text-3xl font-bold text-green-500">{blockchainData.transactions_24h}</p>
+                </div>
+                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
                   <h2 className="text-2xl font-extrabold text-white font-display">Total Transactions</h2>
-                  <p className="text-3xl font-bold text-green-500">{marketData.blockchain_stats_24_hours?.transaction_count}</p>
+                  <p className="text-3xl font-bold text-green-500">{blockchainData.transactions}</p>
                 </div>
-                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 h-52 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+                <div className="bg-[#766387]/30 hover:bg-[#87729c]/30 backdrop-blur-lg backdrop-brightness-125 p-8 rounded-lg shadow-lg transition-transform transform hover:scale-105">
                   <h2 className="text-2xl font-extrabold text-white font-display">Difficulty</h2>
-                  <p className="text-3xl font-bold text-green-500">{marketData.blockchain_stats_24_hours?.difficulty}</p>
+                  <p className="text-3xl font-bold text-green-500">{blockchainData.difficulty}</p>
                 </div>
               </div>
             </div>
